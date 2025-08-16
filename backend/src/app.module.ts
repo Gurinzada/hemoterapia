1225b1ba32a27064a70653c90e0668deed6ab5f1
Augusto <augustoinacio243@gmail.com>
2025-08-16 17:20:02 -0300

    feat: configure TypeORM with entities and import necessary modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ClientsModule } from './clients/clients.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import * as dotenv from "dotenv";
+import { Appointment } from './appointment/entities/appointment.entity';
+import { User } from './users/entities/user.entity';
+import { Client } from './clients/entities/client.entity';
+dotenv.config();
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -11,10 +20,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_NAME,
       password: process.env.PASSWORD,
       database: process.env.DATABASE_NAME,
-      entities: [],
+      entities: [Appointment, User, Client],
       synchronize: true,
       timezone: 'Z',
     }),
+    UsersModule,
+    ClientsModule,
+    AppointmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
