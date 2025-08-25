af568412750b542a2b5108bb6a7c9225069b9ea2
Augusto <augustoinacio243@gmail.com>
2025-08-25 03:14:52 -0300

    feat: updating PrivateRoute and Rotues

--- frontend/src/components/privateRouter/PrivateRoute.tsx
+++ frontend/src/components/privateRouter/PrivateRoute.tsx
@@ -1,13 +1,38 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/store";
+import { clearStateUser, fetchMyProfileUser } from "../../store/slices/userSlice";
+import Loading from "../Loading/Loading";
+import { useNavigate } from "react-router-dom";
+
 interface PrivateRouteProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export default function PrivateRoute({children}:PrivateRouteProps){
+export default function PrivateRoute({ children }: PrivateRouteProps) {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-    
+  const { loading } = useAppSelector((state) => state.user);
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      const result = await dispatch(fetchMyProfileUser());
 
-    return (
-        {children}
-    )
+      if (fetchMyProfileUser.rejected.match(result)) {
+        dispatch(clearStateUser());
+        localStorage.removeItem("token");
+        navigate("/");
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading === "pending") {
+    return <Loading />;
+  }
+
+  console.log(loading);
+
+  return <>{children}</>;
 }
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/login/Login";
-
-
+import Home from "./pages/home/Home";
+import PrivateRoute from "./components/privateRouter/PrivateRoute";
+import Client from "./pages/clients/Client";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login/>,
-    }
-])
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: (
+      <PrivateRoute>
+        <Home />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/clients",
+    element: (
+      <PrivateRoute>
+        <Client/>
+      </PrivateRoute>
+    )
+  }
+]);
