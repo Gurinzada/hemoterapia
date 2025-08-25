826e0964f20dccbb47de014b48adab9fa3b692a6
Augusto <augustoinacio243@gmail.com>
2025-08-25 03:13:27 -0300

    feat: starting home page

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import Container from "../../components/Container";
+import Header from "../../components/Header/Header";
+export default function Home(){
+
+
+    return(
+        <>
+            <Header/>
+            <Container>
+                <main style={{}}>
+                </main>
+            </Container>
+        </>
+    )
+}
