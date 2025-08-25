62b55505561dd9ece92f648d07ee93c37ecdb92a
Augusto <augustoinacio243@gmail.com>
2025-08-25 03:13:12 -0300

    feat: login page

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -3,9 +3,13 @@ import Container from "../../components/Container";
 import useToast from "../../hooks/useToast";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button, Card, Input } from "@mantine/core";
+import { IconAt, IconPassword } from "@tabler/icons-react";
+import sering from "../../assets/syringe.png";
 
 export default function Login() {
   const { showToastSucess, showToastError, showToastWarn } = useToast();
+  const [loading, setLoading] = useState<boolean>(false);
   const [userInfos, setUserInfos] = useState({
     email: "",
     password: "",
@@ -19,6 +23,7 @@ export default function Login() {
     if (isEmpty) {
       return showToastWarn("Complete os campos para realizar o Login");
     }
+    setLoading(true);
     try {
       e.preventDefault();
       const response = await axios.post(
@@ -36,11 +41,13 @@ export default function Login() {
       if (response.status === 201) {
         localStorage.setItem("token", response.data.token);
         showToastSucess("Login realizado com sucesso");
+        setLoading(false);
         setTimeout(() => {
           navigate("/home");
         }, 4500);
       }
     } catch {
+      setLoading(false);
       return showToastError("Email ou Senha incorretas");
     }
   };
@@ -48,7 +55,46 @@ export default function Login() {
   return (
     <>
       <Container>
-        <div></div>
+        <main style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+          <Card shadow="sm" radius={"md"} padding={"xl"} style={{display: "flex", gap: "1rem"}}>
+            <Card.Section>
+              <div style={{textAlign: "center"}}>
+                <img src={sering} alt="" style={{width: "20%"}}/>
+                <h2>Bem vindo a sua Agenda</h2>
+                <h3>Faça seu Login</h3>
+              </div>
+            </Card.Section>
+            <Card.Section>
+              <div style={{padding: "1rem"}}>
+                <Input
+                type="email"
+                placeholder="Digite seu email"
+                leftSection={<IconAt size={16} />}
+                onChange={(e) => setUserInfos({
+                  ...userInfos,
+                  email: e.target.value 
+                })}
+              />
+              </div>
+              <div style={{padding: "1rem"}}>
+                <Input
+                  type="password"
+                  placeholder="Digite sua senha"
+                  leftSection={<IconPassword size={16}/>}
+                  onChange={(e) => setUserInfos({
+                    ...userInfos,
+                    password: e.target.value
+                  })}
+                />
+              </div>
+            </Card.Section>
+            <Card.Section>
+              <div style={{padding: "1rem"}}>
+                <Button disabled={loading} onClick={handleSubmitLogin} color="teal" variant="outline" fullWidth>Login</Button>
+              </div>
+            </Card.Section>
+          </Card>
+        </main>
       </Container>
     </>
   );
