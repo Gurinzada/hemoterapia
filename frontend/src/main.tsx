9a4f59f2a95b0a50461701bb25aceba4605f8990
Augusto <augustoinacio243@gmail.com>
2025-08-30 04:17:14 -0300

    feat: updating main.tsx to get css styles from @matine/dates/styles.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,19 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import "@mantine/core/styles.css";
+import '@mantine/dates/styles.css';
+import "dayjs/locale/pt-br"
 import { MantineProvider } from "@mantine/core";
 import { ToastContainer } from "react-toastify";
+import { DatesProvider } from "@mantine/dates";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <MantineProvider withGlobalClasses>
-        <App />
+        <DatesProvider settings={{ locale: "pt-br" }}>
+          <App />
+        </DatesProvider>
       </MantineProvider>
       <ToastContainer/>
     </Provider>
